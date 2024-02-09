0588e6108691c28d8ac5191e9807f655c0e0d991
DocBarnecle <barnecled@gmail.com>
2024-02-10 01:48:42 +0300

    http allow ip address is changed.

--- ArcelikWebApi/ArcelikWebApi/obj/Debug/net7.0/ArcelikWebApi.AssemblyInfo.cs
+++ ArcelikWebApi/ArcelikWebApi/obj/Debug/net7.0/ArcelikWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArcelikWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2d2123de3005bd2f955942d15360d26c618fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb17d3188b1d85c3bc571e74eda6448a473d8dc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArcelikWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArcelikWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
